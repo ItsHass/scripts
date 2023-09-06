af72484a7163704ed2a7355f699569d8f55192ed
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-09-06 11:18:18 +0100

    Add files via upload

---
+++ windows/powershells/active-directory/GetADGroupMemershipList.ps1
@@ -0,0 +1,9 @@
+clear
+$UserID = Read-Host -Prompt 'Please Enter Username'
+
+Get-ADPrincipalGroupMembership -Identity $UserID | select SamAccountName
+
+pause
+
+
+& "$PSScriptRoot\GetADGroupMemershipList.ps1"
