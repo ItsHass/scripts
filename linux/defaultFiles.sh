485cb2059eaf3f96899538a451b1063ad7025e11
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-11-08 14:24:28 +0000

    Create defaultFiles.sh

---
+++ linux/defaultFiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/ItsHass/scripts/main/linux/docker-add-normal-user.sh
+wget https://raw.githubusercontent.com/ItsHass/scripts/main/linux/set-hostname.sh
+wget https://raw.githubusercontent.com/ItsHass/scripts/main/linux/promox-install-qemu-guest.sh
+chmod 777 *
