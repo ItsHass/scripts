97448ed9594ece53a02e559f5e8326f4f3bc7230
ItsHass <88739447+ItsHass@users.noreply.github.com>
2024-03-22 22:30:29 +0000

    Update server-step1.sh

--- linux/wireguard-setup/server-step1.sh
+++ linux/wireguard-setup/server-step1.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install wireguard
+sudo apt install wireguard -y
 wg genkey | sudo tee /etc/wireguard/private.key
 sudo chmod go= /etc/wireguard/private.key
 sudo cat /etc/wireguard/private.key | wg pubkey | sudo tee /etc/wireguard/public.key
