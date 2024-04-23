880806e4d91f74f9f9eaa699fc83157bd4e5d993
ItsHass <88739447+ItsHass@users.noreply.github.com>
2024-04-23 11:25:22 +0100

    Update new-identity.sh

--- storj-setup/new-identity.sh
+++ storj-setup/new-identity.sh
@@ -14,10 +14,10 @@ echo Identity Created.
 
 echo Please Authorise.
 read -p "Auth Key: " AuthKey
-identity authorize storagenode $AuthKey
+identity authorize $NodeName $AuthKey
 
-grep -c BEGIN ~/.local/share/storj/identity/storagenode/ca.cert
-grep -c BEGIN ~/.local/share/storj/identity/storagenode/identity.cert
+grep -c BEGIN ~/.local/share/storj/identity/$NodeName/ca.cert
+grep -c BEGIN ~/.local/share/storj/identity/$NodeName/identity.cert
 
 echo "2 and 3 shoudl be shown above to confirm identity is authorised."
 echo Storj Identity Setup Complete.
