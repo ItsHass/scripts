d947cc9a39843f6019c9bf0b339b053636e3aa23
ItsHass <88739447+ItsHass@users.noreply.github.com>
2024-04-29 08:55:00 +0100

    Update standard-04-2024.sh

--- storj-setup/standard-04-2024.sh
+++ storj-setup/standard-04-2024.sh
@@ -18,6 +18,10 @@ ExtAddr=$Domain:$Windscribe_Port
 IdentityLocation=/mnt/location
 StorageLocation=/mnt/location
 
+dbs_location=/mnt/storj_dbs/dbs/
+storage2_dbs=dbs
+storage2_orders=dbs/orders
+
 MaxLogSize=10m
 
-docker run -d --restart unless-stopped --stop-timeout 300 --log-opt max-size=$MaxLogSize -p $Port:28967/udp -p $Port:28967/tcp -p $DashPort:14002 -e WALLET="$Wallet" -e EMAIL="$Email" -e ADDRESS="$ExtAddr" -e BANDWIDTH="1000TB" -e STORAGE="$NodeSize" --mount type=bind,source="$IdentityLocation",destination=/app/identity --mount type=bind,source="$StorageLocation",destination=/app/config --sysctl net.ipv4.tcp_fastopen=3 --name $DockerName storjlabs/storagenode:latest --operator.wallet-features=zksync --pieces.enable-lazy-filewalker="true"
+docker run -d --restart unless-stopped --stop-timeout 300 --log-opt max-size=$MaxLogSize -p $Port:28967/udp -p $Port:28967/tcp -p $DashPort:14002 -e WALLET="$Wallet" -e EMAIL="$Email" -e ADDRESS="$ExtAddr" -e BANDWIDTH="1000TB" -e STORAGE="$NodeSize" --mount type=bind,source="$IdentityLocation",destination=/app/identity --mount type=bind,source="$StorageLocation",destination=/app/config --mount type=bind,source="$dbs_location",destination=/app/dbs --sysctl net.ipv4.tcp_fastopen=3 --name $DockerName storjlabs/storagenode:latest --operator.wallet-features=zksync --pieces.enable-lazy-filewalker="true"
