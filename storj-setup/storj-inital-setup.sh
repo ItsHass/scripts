296f343aed80b289c56c8e005b365f46393a875b
ItsHass <88739447+ItsHass@users.noreply.github.com>
2024-04-30 16:46:00 +0100

    Update storj-inital-setup.sh

--- storj-setup/storj-inital-setup.sh
+++ storj-setup/storj-inital-setup.sh
@@ -1,11 +1,12 @@
 identityLoc=
 storageLoc=
 dbs_location=
+storj_name=
 
 docker run --rm -e SETUP="true" \
     --user $(id -u):$(id -g) \
     --mount type=bind,source="$identityLoc",destination=/app/identity \
     --mount type=bind,source="$storageLoc",destination=/app/config \
     --mount type=bind,source="$dbs_location",destination=/app/dbs \
-    --name storagenode storjlabs/storagenode:latest
+    --name $storj_name storjlabs/storagenode:latest
 
