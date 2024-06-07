3c98efd32171c0c1ef5e7922121de90c95472424
ItsHass <88739447+ItsHass@users.noreply.github.com>
2024-06-07 12:52:42 +0100

    Update 1_storj_db_orders_rsync.sh

--- storj-db-merg/1_storj_db_orders_rsync.sh
+++ storj-db-merg/1_storj_db_orders_rsync.sh
@@ -1,14 +1,15 @@
-from=/mnt/storj../storage/
-from1=/mnt/storj../orders/
+maindir=/mnt/storj...
+from=$maindir/storage/
+from1=$maindir/orders/
 
 to=/mnt/storj_dbs/dbs
 to1=/mnt/storj_dbs/dbs/orders
 
-#rm -r /mnt/storj-4tb_dbs_orders/*
+###rm -r /mnt/storj-4tb_dbs_orders/*
 mkdir -p $to
 mkdir -p $to1
 
 
-#rsync -avP -m --include="*.db" --exclude="*" "$from" "$to"
+###rsync -avP -m --include="*.db" --exclude="*" "$from" "$to"
 rsync -aruvP -m --exclude='*/' "$from" "$to"
 rsync -aruvP "$from1" "$to1"
