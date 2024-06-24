77341d11ddba5a3a8499642098cc402f821c5445
ItsHass <88739447+ItsHass@users.noreply.github.com>
2024-06-24 22:29:55 +0100

    .

---
+++ storj-automated-config/variables.sh
@@ -0,0 +1,26 @@
+# variables.sh
+
+DockerRemoveName=storagenode
+DockerName=storagenode
+
+NodeSize=3.5TB
+Port=289x
+DashPort=14002
+Wallet=xxxxxxxxxxxxxxxxx
+Email=xxxxxxxxxxxxxxxxxx
+
+Domain=....
+Windscribe_Port=....
+ExtAddr=$Domain:$Windscribe_Port
+
+IdentityLocation=/mnt/location
+StorageLocation=/mnt/location
+
+dbs_location=/mnt/storj_dbs/dbs/
+storage2_dbs=dbs
+storage2_orders=dbs/orders
+
+MaxLogSize=10m
+LogFilePath="/mnt/logs/storagenode/"
+
+LogLevel=error
