6e02c9751006a5d306e7140fda950b5773b02628
ItsHass <88739447+ItsHass@users.noreply.github.com>
2025-01-23 22:24:17 +0000

    update standard storj sh

--- storj-setup/standard-07-2024.sh
+++ storj-setup/standard-07-2024.sh
@@ -27,6 +27,8 @@ MaxLogSize=10m
 LogLevel=error
 CustomLog=piecestore=WARN
 
-docker run -d --restart unless-stopped --stop-timeout 300 --log-opt max-size=$MaxLogSize -p $Port:28967/udp -p $Port:28967/tcp -p $DashPort:14002 -e WALLET="$Wallet" -e EMAIL="$Email" -e ADDRESS="$ExtAddr" -e BANDWIDTH="1000TB" -e STORAGE="$NodeSize" --mount type=bind,source="$IdentityLocation",destination=/app/identity --mount type=bind,source="$StorageLocation",destination=/app/config --mount type=bind,source="$dbs_location",destination=/app/dbs --sysctl net.ipv4.tcp_fastopen=3 --name $DockerName storjlabs/storagenode:latest --storage2.database-dir=$storage2_dbs --storage2.orders.path=$storage2_orders --log.level=$LogLevel --log.custom-level=$CustomLog --pieces.enable-lazy-filewalker="true"
+HashStoreDIR=/mnt/disk
+
+docker run -d --restart unless-stopped --stop-timeout 300 --log-opt max-size=$MaxLogSize -p $Port:28967/udp -p $Port:28967/tcp -p $DashPort:14002 -e WALLET="$Wallet" -e EMAIL="$Email" -e ADDRESS="$ExtAddr" -e BANDWIDTH="1000TB" -e STORAGE="$NodeSize" --mount type=bind,source="$HashStoreDIR",destination=/app/config/storage/hashstore --mount type=bind,source="$IdentityLocation",destination=/app/identity --mount type=bind,source="$StorageLocation",destination=/app/config --mount type=bind,source="$dbs_location",destination=/app/dbs --sysctl net.ipv4.tcp_fastopen=3 --name $DockerName storjlabs/storagenode:latest --storage2.database-dir=$storage2_dbs --storage2.orders.path=$storage2_orders --log.level=$LogLevel --log.custom-level=$CustomLog --pieces.enable-lazy-filewalker="true"
 
 docker logs $DockerName -f --tail 10
