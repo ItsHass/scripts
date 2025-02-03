e643a360d874d3dcdd94aff30ee6f7f08ae8ecb9
ItsHass <88739447+ItsHass@users.noreply.github.com>
2025-02-03 22:09:19 +0000

    Create wireguard_server_opt_share_network.sh

---
+++ linux/wireguard-setup/wireguard_server_opt_share_network.sh
@@ -0,0 +1,3 @@
+# Optional: NAT configuration to allow traffic to the internet
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o ens6 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o ens6 -j MASQUERADE
